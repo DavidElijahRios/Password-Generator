fffd81bd18037e6daf2ab157d0475d2cb5fef0dd
David Rios <david.riostech@outlook.com>
2022-04-09 08:16:58 -0700

    added Read.me and mapped out plan for JS funtionality of password generator

---
+++ Develop/script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+
+//TODO need to make a global variable to store letters and characters
+
+var generateBtn = document.querySelector("#generate");
+
+//TODO need to write a function named writePassword to run below
+// function: Plan build out below
+
+// need a confirm method to user to see if they want lowercase, uppercase, numbers, and or special characters
+// need to capture user input in variables 
+// need to use a prompt method to ask user how many characters they want for password min 8 max 128 characters
+
+// once submitted password needs to be generated in alert window or in password generator box.
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
