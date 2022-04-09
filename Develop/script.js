250cc9235a45b69a5035967d0c28d268441d44f1
David Rios <david.riostech@outlook.com>
2022-04-09 08:30:12 -0700

    added global variables and start of function in JS code

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,19 @@
 
 //TODO need to make a global variable to store letters and characters
 
+var lowercase = "qwertyuiopasdfghjklzxcvbnm";
+var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var numbers = "0123456789";
+var special = "!#$%&)(*+-./:;?@[]^_{}~";
+
 var generateBtn = document.querySelector("#generate");
 
 //TODO need to write a function named writePassword to run below
+
+var generatePassword = function () {
+
+}
+
 // function: Plan build out below
 
 // need a confirm method to user to see if they want lowercase, uppercase, numbers, and or special characters
@@ -12,6 +22,7 @@ var generateBtn = document.querySelector("#generate");
 // need to use a prompt method to ask user how many characters they want for password min 8 max 128 characters
 
 // once submitted password needs to be generated in alert window or in password generator box.
+
 
 // Write password to the #password input
 function writePassword() {
